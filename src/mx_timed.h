771d331c5ce1120e98d2258a79a868dd734ef63d
joy tu <joy.tu@moxa.com>
2021-11-02 09:47:47 +0800

    progress: Remove unnecessary .c/.h file.
    Done List
    ------------------------------------------
    Done mx_cert_mgmt_main.c
    Done mx_cert_mgmt_lib.c (includes certificate/ encrypt/ decrypt)
    Done mx_cert_mgmt_lib.h
    Done test_mx_cert_lib.c
    Done CMakeLists for packing deb
    Done Regen(POST), Get CertInfo (GET), Delete Import Cert (DELETE)
    Done Import (POST)

--- src/mx_timed.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MX_TIMED_H__
-#define __MX_TIMED_H__
-
-int timed_init(const char *config_path, const char *defconfig_path);
-
-void timed_main(void);
-
-#endif //__MX_TIMED_H__
--- src/mx_timed_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MX_TIMED_CONFIG__
-#define  __MX_TIMED_CONFIG__
-
-int timed_config_init();
-
-int timed_config_deinit();
-
-#endif //__MX_TIMED_CONFIG__
