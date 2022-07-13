4f436c400ac9efc0cd9f5069b283f01fdded7de4
Claire Chen <claireny.chen@moxa.com>
2022-07-13 17:27:32 +0800

    ci: add coverity and valgrind

---
+++ scripts/valgrind-memcheck.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd $(dirname $0)
+mkdir -p ../log/valgrind_log
+
+make all -C ..
+valgrind --leak-check=yes --error-exitcode=1 --keep-debuginfo=yes --log-file=../log/valgrind_log/valgrind_mx-cert-mgmtd.log ../platform/laputa/bin/mx-cert-mgmtd &
+
+valgrind_pid=$!
+
+echo sleep 3s......  
+sleep 3
+
+#----- add something here to interact with the daemon -----#
+
+#----- add something here to interact with the daemon end -----#
+
+killall memcheck-amd64-
+wait $valgrind_pid
+valgrind_exit_code=$?
+
+if [ $valgrind_exit_code -eq 0 ]
+then
+  echo [PASSED]
+  exit 0
+else
+  echo [FAILED]. exit code:$valgrind_exit_code
+  exit $valgrind_exit_code
+fi
