4cc5cbe68b7d950c42ae929b6b13e66621ae8764
Claire Chen <claireny.chen@moxa.com>
2022-11-09 14:44:15 +0800

    fix: valgrind mkdir and install deb

--- scripts/valgrind-memcheck.sh
+++ scripts/valgrind-memcheck.sh
@@ -3,6 +3,9 @@
 cd $(dirname $0)
 mkdir -p ../log/valgrind_log
 
+mkdir -p /run/tmp
+./../platform/laputa/scripts/build.sh
+dpkg -i ../platform/laputa/build/*.deb
 make debug -C ..
 valgrind --leak-check=yes --error-exitcode=1 --keep-debuginfo=yes --log-file=../log/valgrind_log/valgrind_mx-cert-mgmtd.log ../platform/laputa/bin/mx-cert-mgmtd &
 
